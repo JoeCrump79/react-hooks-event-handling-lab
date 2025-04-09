3b0ef5c568ecab349ce13b9d62870dfdf7ca5b55
Joseph Crump <josephcrump79@yahoo.com>
2025-04-09 12:43:00 -0500

    react-hooks-event-handling-lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
+import React from 'react';
+
+function EyesOnMe() {
+  function handleFocus() {
+    console.log('Good!');
+  }
+
+  function handleBlur() {
+    console.log('Hey! Eyes on me!');
+  }
+
+  return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
 // Code EyesOnMe Component Here
